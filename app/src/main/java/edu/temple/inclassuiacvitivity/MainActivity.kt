3dadfadf7326269e2c88748c9f698a77bc862588
AndrewDKahn <18akahn.stem@gmail.com>
2022-09-15 14:37:44 -0400

    finished??

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -10,13 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var size = 2f
+
         val spinner = findViewById<Spinner>(R.id.spinner)
         val changeButton = findViewById<Button>(R.id.changeButton)
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         // Step 1: Populate array
         val numberArray = IntArray(100)
-
+        for (i in 1..100){
+            numberArray[i-1] = i
+        }
         spinner.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, numberArray.asList())
 
 
@@ -28,7 +32,7 @@ class MainActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-
+                size = parent?.getItemAtPosition(position).toString().toFloat()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {}
@@ -36,7 +40,7 @@ class MainActivity : AppCompatActivity() {
 
         // Step 3: Change TextView to saved text size
         changeButton.setOnClickListener {
-
+            displayTextView.textSize = size
         }
 
     }
